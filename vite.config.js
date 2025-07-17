c4c6b3a3130e1a6910e3b1423a88c2c505dec442
David Hernandez Lopez <david.hernandez-lopez@capgemini.com>
2025-07-17 15:56:50 -0400

    [ADD] gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// shoppingreact
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/sn_paradise_nursery_shopping",
   plugins: [react()],
 })
